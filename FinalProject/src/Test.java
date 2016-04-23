67c6ec9abc3dea1654b460f051569cf1b6347e2a
bfpri <jeremy4tzou@gmail.com>
2016-04-22 21:09:29 -0500

    added jars for solr

--- FinalProject/src/Test.java
+++ FinalProject/src/Test.java
@@ -1,3 +1,13 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+
 
 public class Test {
 	public static void main(String[] args){
